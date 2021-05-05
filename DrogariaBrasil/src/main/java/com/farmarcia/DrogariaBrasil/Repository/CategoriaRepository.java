f16e66fb7a328585db478f496010232e7dbfe188
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-05-04 21:23:48 -0300

    Correção de bugs

--- DrogariaBrasil/src/main/java/com/farmarcia/DrogariaBrasil/Repository/CategoriaRepository.java
+++ DrogariaBrasil/src/main/java/com/farmarcia/DrogariaBrasil/Repository/CategoriaRepository.java
@@ -3,9 +3,11 @@ package com.farmarcia.DrogariaBrasil.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.farmarcia.DrogariaBrasil.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
 	public List<Categoria> findAllByTituloContainingIgnoreCase (String titulo); 
--- DrogariaBrasil/src/main/java/com/farmarcia/DrogariaBrasil/Repository/ProdutoRepository.java
+++ DrogariaBrasil/src/main/java/com/farmarcia/DrogariaBrasil/Repository/ProdutoRepository.java
@@ -3,9 +3,11 @@ package com.farmarcia.DrogariaBrasil.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.farmarcia.DrogariaBrasil.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 	public List<Produto> findAllByDescricaoContainingIgnoreCase (String descricao); 
 
